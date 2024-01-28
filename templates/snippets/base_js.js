a358ffad3608c5d7c0739ab658ffe88b7c668f6f
Zaid Nasiruddin Ansari <125444268+Zaid-N-Ansari@users.noreply.github.com>
2024-01-28 20:12:04 +0530

    Add files via upload

---
+++ templates/snippets/base_js.js
@@ -0,0 +1,40 @@
+function preloadCallback(src, elmID) {
+	const img = document.getElementById(elmID);
+	img.src = src;
+}
+
+function preloadImg(imgSrc, elmID) {
+	const ImagePreload = new Image();
+	ImagePreload.src = imgSrc;
+
+	if (ImagePreload.complete) {
+		preloadCallback(ImagePreload.src, elmID);
+		ImagePreload.onload = function () { }
+	}
+	else {
+		ImagePreload.onload = function () {
+			preloadCallback(ImagePreload.src, elmID);
+			ImagePreload.onload = function () { }
+		}
+	}
+}
+
+function validateText(str) {
+	const md = window.markdownit({
+		highlight: function (str, lang) {
+			if (lang && hljs.getLanguage(lang)) {
+				try {
+					return '<pre><code class="hljs">' +
+						hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+						'</code></pre>';
+				} catch (__) { }
+			}
+
+			return '<pre><code class="hljs">' + md.utils.escapeHtml(str) + '</code></pre>';
+		},
+		linkify: true,
+	});
+
+	const result = md.render(str);
+	return result;
+}
